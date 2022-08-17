804f3c4efa1649d4108dd566b5bd181a1d1b4077
Ivan <i.shapoval@samsung.com>
2022-08-17 16:40:50 +0300

    add tests for LPVSFileTest

---
+++ src/test/java/com/lpvs/entity/LPVSFileTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2022, Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package com.lpvs.entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LPVSFileTest {
+
+    @Test
+    public void convertLicensesToStringTest() {
+        assertEquals(true, true);
+    }
+}
